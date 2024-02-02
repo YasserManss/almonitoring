21624b5cac31ea357d93f831630b072e7ffabb5a
YasserManss <yaser775358@gmail.com>
2024-02-02 11:19:48 +0300

    redis config and start/stop script

---
+++ bin/redis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "%1" ]; then
+  echo "Usage: ./script.sh [start|stop]"
+  echo "start: start redis as daemon"
+  echo "stop: stop redis"
+elif [ "$1" == "start" ]; then
+  cd "$(dirname "$0")"
+  redis-server ../conf/redis.conf  --daemonize yes
+elif [ "$1" == "stop" ]; then
+  redis-cli shutdown
+else
+  echo "Usage: ./script.sh [start|stop]"
+  echo "start: start redis as daemon"
+  echo "stop: stop redis"
+fi
