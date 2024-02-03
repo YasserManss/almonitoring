969229e04d912152f85bb261d100ce2062aec128
YasserManss <yaser775358@gmail.com>
2024-02-03 17:59:31 +0300

    added service files example

--- ingress/rest_api/rest_api.py
+++ ingress/rest_api/rest_api.py
@@ -1,12 +1,17 @@
 from typing import Annotated
-
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-
+from pydantic import BaseModel, ValidationError, Json
 app = FastAPI()
 security = HTTPBasic()
 
 allowed_users = [('admin','0')]
+
+class Alert(BaseModel):
+    key: str | None = None
+    alert: dict | Json
+
+
 
 
 @app.get("/")
@@ -15,3 +20,10 @@ async def root(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
         raise HTTPException(status_code=401, detail='Not Allowed')
     else:
         return {"message": "Hello World!"}
+
+@app.post("/alert")
+async def create_item(credentials: Annotated[HTTPBasicCredentials, Depends(security)], alert: Alert):
+    if (str(credentials.username), str(credentials.password)) not in allowed_users:
+        raise HTTPException(status_code=401, detail='Not Allowed')
+    else:
+        return alert
